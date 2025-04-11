3399de8e333997a7fe9aa9546b0196718d51874b
Dinhan123456 <chimbuon3@gmail.com>
2025-04-11 14:11:21 +0700

    baitap1

---
+++ lesson8-bien-kieu-du-lieu-toan-tu/baitap-bienvakieudulieutoantu.js
@@ -0,0 +1,7 @@
+
+    let p = prompt("physics");
+    let b = prompt("biology");
+    let c = prompt("chemistry");
+    let t = Number(p) +Number (b) +Number (c);
+    let a = t/3;
+    document.write("điểm TB: " + a);
