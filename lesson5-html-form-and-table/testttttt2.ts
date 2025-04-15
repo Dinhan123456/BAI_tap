660acc11177c8cae8158050b9cddfcf396b383ce
Dinhan123456 <chimbuon3@gmail.com>
2025-04-15 08:21:52 +0700

    bai

---
+++ lesson5-html-form-and-table/testttttt2.ts
@@ -0,0 +1,29 @@
+
+.white {
+    color: white;
+}
+.black {
+    color: black;
+}
+body {
+    background-color: white;
+    padding: 30px;
+    display: flex;
+    justify-content: center;
+}
+table.chessboard{
+    border-collapse: collapse;
+    border: 15px solid orange;
+}
+.label {
+    color: black;
+    background-color: orange;
+    font-weight: bold;
+}
+.chessboard td {
+    width: 50px;
+    height: 50px;
+    text-align: center;
+    vertical-align: middle;
+    font-weight: bold;
+}
--- lesson7-bien-kieu-du-lieu-toan-tu/baitap1.2.js
+++ lesson7-bien-kieu-du-lieu-toan-tu/baitap1.2.js
@@ -1,4 +1,4 @@
 //viet chuong trinh nhap gia tri do C
 let celsius = prompt("Celsius: ");
 let f = (celsius / 5) * 9 + 32
-document.write("độ f: "+f)
+document.write("độ f: "+ f)
